0a9b61051e609c1d83aae23986afc33fd5fc72b0
benweizhu <benweizhu@gmail.com>
2020-08-12 11:47:33 +0800

    add lombok

--- src/main/java/me/zeph/spring/mybatis/springmybatisexample/model/MyCouponTicket.java
+++ src/main/java/me/zeph/spring/mybatis/springmybatisexample/model/MyCouponTicket.java
@@ -1,7 +1,16 @@
 package me.zeph.spring.mybatis.springmybatisexample.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class MyCouponTicket {
 
   private long id;
@@ -9,53 +18,4 @@ public class MyCouponTicket {
   private int type;
   private int createdBy;
   private LocalDateTime createdTime;
-
-  public MyCouponTicket() {
-  }
-
-  public MyCouponTicket(long id, String code, int type) {
-    this.id = id;
-    this.code = code;
-    this.type = type;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public void setCode(String code) {
-    this.code = code;
-  }
-
-  public int getType() {
-    return type;
-  }
-
-  public void setType(int type) {
-    this.type = type;
-  }
-
-  public int getCreatedBy() {
-    return createdBy;
-  }
-
-  public void setCreatedBy(int createdBy) {
-    this.createdBy = createdBy;
-  }
-
-  public LocalDateTime getCreatedTime() {
-    return createdTime;
-  }
-
-  public void setCreatedTime(LocalDateTime createdTime) {
-    this.createdTime = createdTime;
-  }
 }
