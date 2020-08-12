f447feda577a1f776d95483172966fbfde085715
benweizhu <benweizhu@gmail.com>
2020-08-12 08:57:02 +0800

    lombok not working

--- src/main/java/me/zeph/spring/mybatis/springmybatisexample/model/MyCouponTicket.java
+++ src/main/java/me/zeph/spring/mybatis/springmybatisexample/model/MyCouponTicket.java
@@ -1,16 +1,41 @@
 package me.zeph.spring.mybatis.springmybatisexample.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class MyCouponTicket {
+
   private long id;
   private String code;
   private int type;
+
+  public MyCouponTicket() {
+  }
+
+  public MyCouponTicket(long id, String code, int type) {
+    this.id = id;
+    this.code = code;
+    this.type = type;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public int getType() {
+    return type;
+  }
+
+  public void setType(int type) {
+    this.type = type;
+  }
 }
